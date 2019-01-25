369eb399fa72663180f1c38802b8b9c55063cf89
Kevin de Youngster <kevin.deyoungster@gmail.com>
2019-01-25 14:31:49 +0000

    Added web platform check

--- Assets/Scripts/UI/NetworkManager.cs
+++ Assets/Scripts/UI/NetworkManager.cs
@@ -15,8 +15,10 @@ public class NetworkManager : MonoBehaviour {
 
     void Start()
     {
-        string userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
-        header.Add("User-Agent", userAgent);
+         if(Application.platform != RuntimePlatform.WebGLPlayer){
+            string userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
+            header.Add("User-Agent", userAgent);  
+       } 
     }
   
 
